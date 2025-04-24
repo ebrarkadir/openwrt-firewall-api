c29dee2d4ff60c19a166eb090062e5fd47ffb2b6
ebrarkadir <cetinebrarkadir@gmail.com>
2025-04-24 20:28:15 +0300

    port_blocking updated

--- utils/buildCommands.js
+++ utils/buildCommands.js
@@ -1,15 +1,23 @@
 function buildPortBlockingCommands({ portRange, protocol }) {
-  const ruleName = `block_${protocol}_${portRange.replace(':', '-')}_${Date.now()}`;
-  return [
-    `uci add firewall rule`,
-    `uci set firewall.@rule[-1].name='${ruleName}'`,
-    `uci set firewall.@rule[-1].src='wan'`,
-    `uci set firewall.@rule[-1].proto='${protocol.toLowerCase()}'`,
-    `uci set firewall.@rule[-1].dest_port='${portRange}'`,
-    `uci set firewall.@rule[-1].target='REJECT'`,
-    `uci commit firewall`,
-    `/etc/init.d/firewall restart`,
-  ];
+  const zones = ['wan', 'lan']; // Her iki zone için
+  const commands = [];
+
+  zones.forEach((zone) => {
+    const ruleName = `block_${protocol}_${portRange.replace(':', '-')}_${zone}_${Date.now()}`;
+    commands.push(
+      `uci add firewall rule`,
+      `uci set firewall.@rule[-1].name='${ruleName}'`,
+      `uci set firewall.@rule[-1].src='${zone}'`,
+      `uci set firewall.@rule[-1].proto='${protocol.toLowerCase()}'`,
+      `uci set firewall.@rule[-1].dest_port='${portRange}'`,
+      `uci set firewall.@rule[-1].target='REJECT'`
+    );
+  });
+
+  commands.push(`uci commit firewall`);
+  commands.push(`/etc/init.d/firewall restart`);
+
+  return commands;
 }
 
 function buildPortForwardingCommands({ sourceIP, destinationIP, sourcePort, destinationPort, protocol }) {
@@ -44,4 +52,3 @@ module.exports = {
   buildPortBlockingCommands,
   buildPortForwardingCommands,
 };
-//uci show firewall | grep redirect
